aed317294933dcafb1e50af3e72e38a979343722
fantistac <l1310724816@163.com>
2023-03-28 20:18:07 +0800

    udp-server-client

--- client/test.c
+++ client/test.c
@@ -12,38 +12,33 @@ int main()
 	struct sockaddr_in server_addr;
 	int fd = 0, ret = 0;
 	char BUF[BUFSIZ];
+	socklen_t server_len;
+	fd = socket(AF_INET, SOCK_DGRAM, 0);
+	if(fd == -1)
+	fprintf(stderr, "socket error: %s\n", strerror(errno));
 
-	fd = socket(AF_INET, SOCK_STREAM, 0);
-	if(fd == -1){
-		fprintf(stderr, "socket error: %s\n", strerror(errno));
-	}
 
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(9527);
-	
+
 	ret = inet_aton("127.0.0.1", &server_addr.sin_addr.s_addr);
 	if(ret == 0){
-		fprintf(stderr, "socket error: %s\n", strerror(errno));
+		fprintf(stderr, "translate error\n");
 	}
 
-	ret = connect(fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-	if(ret == -1)
-		fprintf(stderr, "connect error: %s\n", strerror(errno));
-	
 	while( fgets(BUF, BUFSIZ, stdin) ){
-
-			ret = write(fd, BUF, strlen(BUF));
-			if(ret == -1)
-				fprintf(stderr, "write1 error: %s\n", strerror(errno));
-
-			ret = read(fd, BUF, BUFSIZ);
-			if(ret == -1)
+		int ret = sendto(fd, BUF, strlen(BUF), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+		if(ret == -1)
+				fprintf(stderr, "connect error: %s\n", strerror(errno));
+		else{ 
+				ret = recvfrom(fd, BUF, BUFSIZ, 0, NULL, NULL);
+		if(ret == -1)
 				fprintf(stderr, "read error: %s\n", strerror(errno));
 
-			write(STDOUT_FILENO, BUF, ret);
-			if(ret == -1)
-				fprintf(stderr, "write2 error: %s\n", strerror(errno));
+				write(STDOUT_FILENO, BUF, ret);
+				if(ret == -1)
+						fprintf(stderr, "write2 error: %s\n", strerror(errno));
+		}
 	}
-
-	return 0;
+return 0;
 }
--- test.c
+++ test.c
@@ -12,79 +12,44 @@
 
 int main()
 {
-	int listen_fd, client_fd;
+	int sockfd, client_fd;
 	int client_port;
 	char client_id[BUFSIZ];
 	int ret = 0, rb_fd = 0;
 	struct sockaddr_in server_addr, client_addr;
-	struct epoll_event ev, evs[128];
 	socklen_t client_len;
 	char BUF[BUFSIZ];
 
-	listen_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if(listen_fd == -1) fprintf(stderr, "socket error: %s\n", strerror(errno));
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if(sockfd == -1) fprintf(stderr, "socket error: %s\n", strerror(errno));
 	
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(9527);
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	ret = bind(listen_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+	ret = bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
 	if(ret == -1) fprintf(stderr, "bind error: %s\n", strerror(errno));
 
-	ret = listen(listen_fd, 128);
-	if(ret == -1) fprintf(stderr, "listen error: %s\n", strerror(errno));
-	
-	rb_fd = epoll_create(128);
-	if(rb_fd == -1) fprintf(stderr, "epoll_creat error: %s\n", strerror(errno));
-
-	ev.data.fd = listen_fd;
-	ev.events = EPOLLIN;
-
-	ret = epoll_ctl(rb_fd, EPOLL_CTL_ADD, listen_fd, &ev);
-	if(ret == -1) fprintf(stderr, "epoll_ctl error: %s\n", strerror(errno));
 	
 	while(1)
 	{
-		ret = epoll_wait(rb_fd, &evs, 128, -1);
-  	if(ret == -1) fprintf(stderr, "epoll_wait error: %s\n", strerror(errno));
 
-		if(ret > 0){
-			for(int i = 0; i < ret; ++i){
-			if(evs[i].events != EPOLLIN) continue;
-
-			if(evs[i].data.fd == listen_fd){
-				client_len = sizeof(client_addr);
-				client_fd = accept(listen_fd, (struct sockaddr *)&client_addr, &client_len);
-				if(client_fd == -1) fprintf(stderr, "accept error: %s\n", strerror(errno));
-
+		client_len = sizeof(client_addr);
+		int ret = recvfrom(sockfd, BUF, BUFSIZ, 0, (struct sockaddr *)&client_addr, &client_len);
+		if (ret == -1) fprintf(stderr, "recvfrom error:%s\n", strerror(errno));
+		else{
 				inet_ntop(AF_INET, (void *)&client_addr.sin_addr.s_addr, client_id, BUFSIZ);
 				client_port = ntohs(client_addr.sin_port);
-				fprintf(stdout, "client ip = %s, client port = %d is connecting\n",client_id, client_port);  
+				fprintf(stderr, "client ip = %s, client port = %d is connecting\n",client_id, client_port);  
 
-				ev.data.fd = client_fd;
-				ev.events = EPOLLIN;
-				int tmp = epoll_ctl(rb_fd, EPOLL_CTL_ADD, client_fd, &ev);
-				if(tmp == -1) fprintf(stderr, "epoll_ctl_client error: %s\n", strerror(errno));
-				continue;
-			}else{
-				int count = read (evs[i].data.fd, BUF, BUFSIZ);	
-				if(count == 0){
-					fprintf(stdout, "%d is closed...\n", evs[i].data.fd);
-					close(evs[i].data.fd);
-					epoll_ctl(rb_fd, EPOLL_CTL_DEL,evs[i].data.fd,NULL);
- 				}else if(count == -1) {
-					fprintf(stderr, "read error: %s\n", strerror(errno));
-				}else{
-					write(STDOUT_FILENO, BUF, count);
-					for(int j = 0; j < count; ++j){
+					write(STDOUT_FILENO, BUF, ret);
+					for(int j = 0; j < ret; ++j){
 						BUF[j] = toupper(BUF[j]);
- 					}
-					write(evs[i].data.fd, BUF, count);
- 				}
- 				}
-			}
+ 			 		}
+					sendto(sockfd, BUF, ret, 0, (struct sockaddr *)&client_addr, sizeof(client_addr));
+ 		} 
+ 		
 		}
-	}
 	return 0;
 }
 
