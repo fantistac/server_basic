d09a3dfee21aa01e5f4ae285885e596f300ca6a7
fantistac <l1310724816@163.com>
2023-03-29 15:11:36 +0800

    incorrect_local_socket

--- client/test.c
+++ client/test.c
@@ -6,39 +6,50 @@
 #include<sys/socket.h>
 #include<arpa/inet.h>
 #include<netinet/in.h>
+#include<sys/un.h>
+#include<stddef.h>
+
+#define CLIENT_ADDR "client_file.socket"
+#define SERVER_ADDR "server_file.socket"
 
 int main()
 {
-	struct sockaddr_in server_addr;
+	struct sockaddr_un server_addr, client_addr;
 	int fd = 0, ret = 0;
 	char BUF[BUFSIZ];
-	socklen_t server_len;
-	fd = socket(AF_INET, SOCK_DGRAM, 0);
+	socklen_t server_len, client_len;
+	fd = socket(AF_UNIX, SOCK_STREAM, 0);
 	if(fd == -1)
 	fprintf(stderr, "socket error: %s\n", strerror(errno));
 
+	unlink(CLIENT_ADDR);
 
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(9527);
+	server_addr.sun_family = AF_UNIX;
+	strcpy(server_addr.sun_path, SERVER_ADDR);
+	server_len = offsetof(struct sockaddr_un, sun_path) + strlen(server_addr.sun_path);
+	client_addr.sun_family = AF_UNIX;
+	strcpy(client_addr.sun_path, CLIENT_ADDR);
+	client_len = offsetof(struct sockaddr_un, sun_path) + strlen(client_addr.sun_path);
 
-	ret = inet_aton("127.0.0.1", &server_addr.sin_addr.s_addr);
-	if(ret == 0){
-		fprintf(stderr, "translate error\n");
-	}
+	bind(fd, (struct sockaddr *)&client_addr, client_len);
 
-	while( fgets(BUF, BUFSIZ, stdin) ){
-		int ret = sendto(fd, BUF, strlen(BUF), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+	ret = connect(fd, (struct sockaddr *)&server_addr, server_len);
+	if(ret == -1)
+			fprintf(stderr, "connect error: %s\n", strerror(errno));
+	while( fgets(BUF, BUFSIZ, stdin) ){ 
+		int ret = write(fd, BUF, strlen(BUF));
 		if(ret == -1)
-				fprintf(stderr, "connect error: %s\n", strerror(errno));
+				fprintf(stderr, "write error: %s\n", strerror(errno));
 		else{ 
-				ret = recvfrom(fd, BUF, BUFSIZ, 0, NULL, NULL);
-		if(ret == -1)
-				fprintf(stderr, "read error: %s\n", strerror(errno));
+				ret = read(fd, BUF, BUFSIZ);
+				if(ret == -1)
+					fprintf(stderr, "read error: %s\n", strerror(errno));
 
 				write(STDOUT_FILENO, BUF, ret);
 				if(ret == -1)
-						fprintf(stderr, "write2 error: %s\n", strerror(errno));
-		}
+						fprintf(stderr, "write error: %s\n", strerror(errno));
+		}   
 	}
-return 0;
+	close(fd);
+	return 0;
 }
--- test.c
+++ test.c
@@ -7,49 +7,64 @@
 #include<arpa/inet.h>
 #include<string.h>
 #include<errno.h>
+#include<sys/un.h>
+#include<stddef.h>
 
 #define PORT 9527
-
+#define SERVER_ADDR "server_file.socket"
 int main()
 {
 	int sockfd, client_fd;
 	int client_port;
 	char client_id[BUFSIZ];
 	int ret = 0, rb_fd = 0;
-	struct sockaddr_in server_addr, client_addr;
-	socklen_t client_len;
+	struct sockaddr_un server_addr, client_addr;
+	socklen_t client_len, server_len;
 	char BUF[BUFSIZ];
 
-	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
   if(sockfd == -1) fprintf(stderr, "socket error: %s\n", strerror(errno));
 	
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(9527);
-	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	unlink(SERVER_ADDR);
+	server_addr.sun_family = AF_UNIX;
+	strcpy(server_addr.sun_path, SERVER_ADDR);
 
-	ret = bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+	server_len = offsetof(struct sockaddr_un, sun_path) + strlen(server_addr.sun_path);
+	ret = bind(sockfd, (struct sockaddr *)&server_addr, server_len);
 	if(ret == -1) fprintf(stderr, "bind error: %s\n", strerror(errno));
 
+	listen(sockfd, 20);
 	
 	while(1)
 	{
 
 		client_len = sizeof(client_addr);
-		int ret = recvfrom(sockfd, BUF, BUFSIZ, 0, (struct sockaddr *)&client_addr, &client_len);
-		if (ret == -1) fprintf(stderr, "recvfrom error:%s\n", strerror(errno));
-		else{
-				inet_ntop(AF_INET, (void *)&client_addr.sin_addr.s_addr, client_id, BUFSIZ);
-				client_port = ntohs(client_addr.sin_port);
-				fprintf(stderr, "client ip = %s, client port = %d is connecting\n",client_id, client_port);  
-
-					write(STDOUT_FILENO, BUF, ret);
-					for(int j = 0; j < ret; ++j){
-						BUF[j] = toupper(BUF[j]);
- 			 		}
-					sendto(sockfd, BUF, ret, 0, (struct sockaddr *)&client_addr, sizeof(client_addr));
+		client_fd = accept(sockfd, (struct sockaddr *)&client_addr, &client_len);
+		if(client_fd == -1) {
+			fprintf(stderr, "accept error:\n", strerror(errno));
+			exit(-1);
+		}	
+		client_len -= offsetof(struct sockaddr_un, sun_path);
+		client_addr.sun_path[client_len] ='\0';	
+		fprintf(stderr, "client socket = %s\n", client_addr.sun_path);  
+		ret = read(client_fd, BUF, BUFSIZ);
+		if (ret == -1){
+			fprintf(stderr, "recvfrom error:%s\n", strerror(errno));
+			exit(-1);
+		} 
+		else if(ret == 0){ 
+			fprintf(stderr, "client %s is closed\n", client_addr.sun_path);			 close(client_fd);
+			exit(0);
+		}else{
+		write(STDOUT_FILENO, BUF, ret);
+		for(int j = 0; j < ret; ++j){ 
+				BUF[j] = toupper(BUF[j]);
  		} 
- 		
+		client_len += offsetof(struct sockaddr_un, sun_path);	
+		write(client_fd, BUF, ret);
 		}
+	}
+	close(sockfd);	
 	return 0;
 }
 
